5c9dc53078b1d6b91062dee715a7782202ce8b1e
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-08 17:17:29 +0530

    add some ejs pages

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express= require("express");
 const app=express();
 const mongoose=require('mongoose');
 const Listing=require("./models/listing.js");
-
+const path=require("path");
+const methodOverride=require('method-override');
 
 main()
 .then(()=>{
@@ -15,24 +16,60 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/majorproject');
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
 app.get("/",(req,res)=>{
     res.send("hii satya");
 });
 
+//index route
 app.get("/listings",async (req,res)=>{
-    // let sampleListing=new Listing({
-    //     title:"My New Villa",
-    //     description:"By the Beach",
-    //     price:1200,
-    //     location:"chennai",
-    //     country:"india",
-    // });
-    // await sampleListing.save();
-    // console.log("sample was saved");
-    // res.send("successfull");
-    let listing=await Listing.find();
-    res.send(listing);
+    const allListing=await Listing.find({});
+    res.render("listings/index.ejs",{allListing});
 });
+
+//new Route
+app.get("/listings/new",async(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+app.post("/listings",async(req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    console.log(newListing);
+});
+
+//show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listings=await Listing.findById(id);
+    res.render("listings/show.ejs",{listings});
+});
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listings=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listings});
+});
+
+app.put("/listings/:id",async(req,res)=>{
+     let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
+
 app.listen(3000,()=>{
     console.log("server is listening to port 3000");
 });
