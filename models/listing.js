3d14297490fa97f5b7b982874418a2c7bea2becf
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-10 10:53:23 +0530

    fix some code bugs

--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,25 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     },
     description: String,
     image: {
         type: String,
-        default:"https://unsplash.com/photos/brown-wooden-lounge-chairs-near-pool-surrounded-by-palm-trees-vmIWr0NnpCQ",
-        set:(v)=>
-            v === ""
-            ? "https://unsplash.com/photos/brown-wooden-lounge-chairs-near-pool-surrounded-by-palm-trees-vmIWr0NnpCQ" 
-            : v,
+        default: "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=387&auto=format&fit=crop",
+        set: (v) => {
+            if (!v || v.trim() === "") {
+                return "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=387&auto=format&fit=crop";
+            }
+            return v;
+        },
     },
     price:{
         type:Number,
-    },
+  },
     location:{
         type: String,
     },
@@ -25,5 +27,36 @@ const listingSchema = new Schema({
         type:String,
     },
 });
+
 const Listing = mongoose.model("Listing", listingSchema);
-module.exports=Listing;
+module.exports = Listing;
+
+// const mongoose=require('mongoose');
+// const Schema=mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type:String,
+//         required:true,
+//     },
+//     description: String,
+//     image: {
+//         type: String,
+//         default:"https://unsplash.com/photos/brown-wooden-lounge-chairs-near-pool-surrounded-by-palm-trees-vmIWr0NnpCQ",
+//         set:(v)=>
+//             v === ""
+//             ? "https://unsplash.com/photos/brown-wooden-lounge-chairs-near-pool-surrounded-by-palm-trees-vmIWr0NnpCQ" 
+//             : v,
+//     },
+//     price:{
+//         type:Number,
+//     },
+//     location:{
+//         type: String,
+//     },
+//     country: {
+//         type:String,
+//     },
+// });
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports=Listing;
